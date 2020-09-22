814b4dfceabc7f38cda8f00116764be5e69a45d9
Ahmed Abdulrazzaq <Ahmed.abdulrazzaq44@gmail.com>
2020-09-22 14:56:29 -0400

    added all methods for database class

--- src/com/bookworm/DataBase.java
+++ src/com/bookworm/DataBase.java
@@ -1,5 +1,50 @@
 package com.bookworm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataBase {
-    
+    private int inventoryCount;
+    List<Inventory> inventoryList = new ArrayList<>();
+    public DataBase(Inventory inventory)
+    {
+        this.inventoryList.add(inventory);
+        inventory.setInventoryNumber(inventoryCount++);
+    }
+    public void addInventory(Inventory inventory)
+    {
+        this.inventoryList.add(inventory);
+        inventory.setInventoryNumber(inventoryCount++);
+    }
+    public void removeInventory(int inventoryNumber)
+    {
+
+        if (inventoryNumber >= this.inventoryList.size())
+        {
+            System.out.println("Inventory does not exist");
+        }else
+            {
+                for (Inventory inventory : inventoryList)
+                {
+                    if (inventory.getInventoryNumber() == inventoryNumber)
+                    {
+                        this.inventoryList.remove(inventory);
+                        break;
+
+                    }
+                }
+
+            }
+    }
+    public void printInventories()
+    {
+        for (Inventory inventory : inventoryList)
+        {
+            System.out.println("Inventory Number: " + inventory.getInventoryNumber() + " Quantity: " + inventory.getQuantity() +"\n" +
+                               " Title: " + inventory.getBook().getTitle() + "\n" +
+                               " Author: " + inventory.getBook().getAuthor() + "\n" +
+                               " Year Published: "+ inventory.getBook().getYearPublished()+"\n" +
+                               " Price: " + inventory.getBook().getSellPrice());
+        }
+    }
 }
--- src/com/bookworm/Main.java
+++ src/com/bookworm/Main.java
@@ -6,6 +6,15 @@ public class Main {
         Book java1 = new Book("java1","alex","2010");
         Book java2 = new Book("java2","alex","2012");
         Inventory inventory = new Inventory(java,20,0);
+        Inventory inventory2 = new Inventory(java1,20,0);
+        Inventory inventory3 = new Inventory(java2,20,0);
+        DataBase dataBase = new DataBase(inventory);
+        dataBase.addInventory(inventory2);
+        dataBase.addInventory(inventory3);
+        dataBase.printInventories();
+        System.out.println("===========================================");
+        dataBase.removeInventory(0);
+        dataBase.printInventories();
 
     }
 }
